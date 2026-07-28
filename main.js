79d10618b502af2ba877a2f0082cb14adbbe6503
Dark Knight <dark-knight@Dark-Knight.lan>
2026-07-28 15:41:11 +0300

    Add new songs to playlist

--- main.js
+++ main.js
@@ -116,6 +116,24 @@ let playlist_content = [
         song_btn: document.createElement("button"),
 
     },
+    {
+        name: "the color of you",
+        artist: "alina baraz - floating ft. khalid",
+        current_status: "idle",
+        id: 0,
+        song_src: "Music/Alina Baraz - Floating ft. Khalid (Official Audio) [m8t5mqYO9Yc].webm",
+        song_btn: document.createElement("button"),
+
+    },
+    {
+        name: "less than a lover",
+        artist: "JENNIE",
+        current_status: "idle",
+        id: 0,
+        song_src: "Music/JENNIE - Less than a Lover (Official Video) [_IT83Y_HcAw].webm",
+        song_btn: document.createElement("button"),
+
+    },
 
 ];
 
